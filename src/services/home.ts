9388a9f2cc7d656b4677532cdfc01334378262e3
thanhhuy44 <thhuy444111@gmail.com>
2024-01-22 08:11:23 +0700

    Fix topslide homepage

--- src/services/home.ts
+++ src/services/home.ts
@@ -1,28 +1,25 @@
-import { CheerioAPI } from "cheerio";
+import { CheerioAPI } from 'cheerio';
 
 const topSlides = async ($: CheerioAPI) => {
-  const slides = $("#slider .swiper-wrapper .swiper-slide")
+  const slides = $('#slider .swiper-wrapper .swiper-slide')
     .get()
     .map((slide, index) => {
-      const poster = $(slide).attr("style")?.split("(")[1].split(")")[0];
-      const name = $(".film-title a", slide).text();
-      const link = $(".film-title a", slide).attr("href");
-      const quality = $(".scd-item .quality", slide).text();
-      const duration = $(
-        ".sc-detail .scd-item strong :first-child",
-        slide
-      ).text();
-      const IMDB = $(".sc-detail .scd-item strong :eq(1)", slide).text();
+      const poster = $(slide).attr('style')?.split('(')[1].split(')')[0];
+      const name = $('.film-title a', slide).text();
+      const link = $('.film-title a', slide).attr('href');
+      const quality = $('.scd-item .quality', slide).text();
+      const duration = $('.sc-detail .scd-item:eq(1) strong', slide).text();
+      const IMDB = $('.sc-detail .scd-item:eq(2) strong ', slide).text();
 
-      const genres = $(".sc-detail .scd-item strong a", slide)
+      const genres = $('.sc-detail .scd-item strong a', slide)
         .get()
         .map((genre) => {
           return {
             text: $(genre).text(),
-            link: $(genre).attr("href"),
+            link: $(genre).attr('href'),
           };
         });
-      const description = $(".sc-desc", slide).text();
+      const description = $('.sc-desc', slide).text();
       return {
         poster,
         link,
@@ -38,19 +35,19 @@ const topSlides = async ($: CheerioAPI) => {
 };
 
 const trendingMovies = async ($: CheerioAPI) => {
-  const trendingMovies = $("#trending-movies .film_list-wrap .flw-item")
+  const trendingMovies = $('#trending-movies .film_list-wrap .flw-item')
     .get()
     .map((movie) => {
-      const quality = $(".film-poster .film-poster-quality", movie).text();
-      const poster = $(".film-poster .film-poster-img", movie).attr("data-src");
-      const name = $(".film-detail .film-name a", movie).text();
-      const link = $(".film-detail .film-name a", movie).attr("href");
-      const time = $(".film-detail .fd-infor .fdi-item:eq(0)", movie).text();
+      const quality = $('.film-poster .film-poster-quality', movie).text();
+      const poster = $('.film-poster .film-poster-img', movie).attr('data-src');
+      const name = $('.film-detail .film-name a', movie).text();
+      const link = $('.film-detail .film-name a', movie).attr('href');
+      const time = $('.film-detail .fd-infor .fdi-item:eq(0)', movie).text();
       const duration = $(
-        ".film-detail .fd-infor .fdi-item.fdi-duration",
+        '.film-detail .fd-infor .fdi-item.fdi-duration',
         movie
       ).text();
-      const type = $(".film-detail .fd-infor .fdi-type", movie).text();
+      const type = $('.film-detail .fd-infor .fdi-type', movie).text();
       return {
         poster,
         name,
@@ -66,16 +63,16 @@ const trendingMovies = async ($: CheerioAPI) => {
 };
 
 const trendingTV = async ($: CheerioAPI) => {
-  const trendingTV = $("#trending-tv .film_list-wrap .flw-item")
+  const trendingTV = $('#trending-tv .film_list-wrap .flw-item')
     .get()
     .map((movie) => {
-      const quality = $(".film-poster .film-poster-quality", movie).text();
-      const poster = $(".film-poster .film-poster-img", movie).attr("data-src");
-      const name = $(".film-detail .film-name a", movie).text();
-      const link = $(".film-detail .film-name a", movie).attr("href");
-      const season = $(".film-detail .fd-infor .fdi-item:eq(0)", movie).text();
-      const ep = $(".film-detail .fd-infor .fdi-item:eq(1)", movie).text();
-      const type = $(".film-detail .fd-infor .fdi-type", movie).text();
+      const quality = $('.film-poster .film-poster-quality', movie).text();
+      const poster = $('.film-poster .film-poster-img', movie).attr('data-src');
+      const name = $('.film-detail .film-name a', movie).text();
+      const link = $('.film-detail .film-name a', movie).attr('href');
+      const season = $('.film-detail .fd-infor .fdi-item:eq(0)', movie).text();
+      const ep = $('.film-detail .fd-infor .fdi-item:eq(1)', movie).text();
+      const type = $('.film-detail .fd-infor .fdi-type', movie).text();
 
       return {
         poster,
@@ -93,20 +90,20 @@ const trendingTV = async ($: CheerioAPI) => {
 
 const latestMovies = async ($: CheerioAPI) => {
   const data = $(
-    ".section-id-02:eq(0) .film_list-grid .film_list-wrap .flw-item"
+    '.section-id-02:eq(0) .film_list-grid .film_list-wrap .flw-item'
   )
     .get()
     .map((movie) => {
-      const quality = $(".film-poster .film-poster-quality", movie).text();
-      const poster = $(".film-poster .film-poster-img", movie).attr("data-src");
-      const name = $(".film-detail .film-name a", movie).text();
-      const link = $(".film-detail .film-name a", movie).attr("href");
-      const time = $(".film-detail .fd-infor .fdi-item:eq(0)", movie).text();
+      const quality = $('.film-poster .film-poster-quality', movie).text();
+      const poster = $('.film-poster .film-poster-img', movie).attr('data-src');
+      const name = $('.film-detail .film-name a', movie).text();
+      const link = $('.film-detail .film-name a', movie).attr('href');
+      const time = $('.film-detail .fd-infor .fdi-item:eq(0)', movie).text();
       const duration = $(
-        ".film-detail .fd-infor .fdi-item.fdi-duration",
+        '.film-detail .fd-infor .fdi-item.fdi-duration',
         movie
       ).text();
-      const type = $(".film-detail .fd-infor .fdi-type", movie).text();
+      const type = $('.film-detail .fd-infor .fdi-type', movie).text();
       return {
         poster,
         name,
@@ -123,17 +120,17 @@ const latestMovies = async ($: CheerioAPI) => {
 
 const latestTV = async ($: CheerioAPI) => {
   const data = $(
-    ".section-id-02:eq(1) .film_list-grid .film_list-wrap .flw-item"
+    '.section-id-02:eq(1) .film_list-grid .film_list-wrap .flw-item'
   )
     .get()
     .map((movie) => {
-      const quality = $(".film-poster .film-poster-quality", movie).text();
-      const poster = $(".film-poster .film-poster-img", movie).attr("data-src");
-      const name = $(".film-detail .film-name a", movie).text();
-      const link = $(".film-detail .film-name a", movie).attr("href");
-      const season = $(".film-detail .fd-infor .fdi-item:eq(0)", movie).text();
-      const ep = $(".film-detail .fd-infor .fdi-item:eq(1)", movie).text();
-      const type = $(".film-detail .fd-infor .fdi-type", movie).text();
+      const quality = $('.film-poster .film-poster-quality', movie).text();
+      const poster = $('.film-poster .film-poster-img', movie).attr('data-src');
+      const name = $('.film-detail .film-name a', movie).text();
+      const link = $('.film-detail .film-name a', movie).attr('href');
+      const season = $('.film-detail .fd-infor .fdi-item:eq(0)', movie).text();
+      const ep = $('.film-detail .fd-infor .fdi-item:eq(1)', movie).text();
+      const type = $('.film-detail .fd-infor .fdi-type', movie).text();
 
       return {
         poster,
@@ -150,15 +147,15 @@ const latestTV = async ($: CheerioAPI) => {
 
 const coming = async ($: CheerioAPI) => {
   const data = $(
-    ".section-id-02:eq(2) .film_list-grid .film_list-wrap .flw-item"
+    '.section-id-02:eq(2) .film_list-grid .film_list-wrap .flw-item'
   )
     .get()
     .map((movie) => {
-      const poster = $(".film-poster .film-poster-img", movie).attr("data-src");
-      const name = $(".film-detail .film-name a", movie).text();
-      const link = $(".film-detail .film-name a", movie).attr("href");
-      const time = $(".film-detail .fd-infor .fdi-item:eq(0)", movie).text();
-      const type = $(".film-detail .fd-infor .fdi-type", movie).text();
+      const poster = $('.film-poster .film-poster-img', movie).attr('data-src');
+      const name = $('.film-detail .film-name a', movie).text();
+      const link = $('.film-detail .film-name a', movie).attr('href');
+      const time = $('.film-detail .fd-infor .fdi-item:eq(0)', movie).text();
+      const type = $('.film-detail .fd-infor .fdi-type', movie).text();
       return {
         poster,
         name,
